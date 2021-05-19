0b8020cc64d703d9271e5fce41b3181fbb1eae56
Kevin Nguyen <Kevin.Nguyen0509@gmail.com>
2021-05-19 00:43:39 -0700

    Changed css and moved addItem function to Grocery Model

--- model/groceryListModel.js
+++ model/groceryListModel.js
@@ -11,4 +11,28 @@ async function getAllGroceryList() {
   }
 }
 
-export { getAllGroceryList };
+async function addItem(name, imageString) {
+  try {
+    const response = await fetch(
+      "https://agile-everglades-82259.herokuapp.com/api/v1/grocery/",
+      {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          name: name,
+          image: imageString,
+          isBought: false,
+        }),
+      }
+    );
+
+    return response.json();
+  } catch (err) {
+    console.log("You already have that item");
+  }
+}
+
+export { getAllGroceryList, addItem };
